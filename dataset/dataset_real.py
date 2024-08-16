511caec0263eb8dd33d2854c87ef61bd048aa2b2
jatq <jatq33@foxmail.com>
2024-08-16 15:22:12 +0800

    Python implementaion of DeepSparser
    first upload

---
+++ dataset/dataset_real.py
@@ -0,0 +1,85 @@
+
+import numpy as np
+import math
+from torch.utils.data import Dataset
+from pathlib import Path
+
+
+
+def get_data(config, is_train=True):
+    if is_train:
+        s = np.load(Path(config.real_data_path) / 'high_snr.npy')
+        n = np.load(Path(config.real_data_path) / 'noises.npy')
+        return s, n
+    else:
+        s = np.load(Path(config.real_data_path) / 'low_snr.npy')
+        return s
+
+
+class MyDataset(Dataset):
+    def __init__(self, config, is_train=True):
+        """
+        s: clean signal
+        n: noise
+        config: configuration
+        """
+        self.config = config
+        if is_train:
+            s, n = get_data(config, is_train=True)
+            self.s = self.normalization_1d(self._pad(s))[0] 
+            self.n = self.normalization_1d(self._pad(n))[0]
+
+        else:
+            s = get_data(config, is_train=False)
+            self.s = self.normalization_1d(self._pad(s))[0]
+
+            
+    def __len__(self):
+        return len(self.s)
+    
+    def __getitem__(self, idx):
+        raise NotImplementedError
+    
+    @staticmethod
+    def normalization_1d(x):
+        mean = np.mean(x, axis=-1, keepdims=True)
+        std = np.std(x, axis=-1, keepdims=True)
+        x = (x-mean)/std
+        return x, std
+    
+    def _pad(self, x):
+        width, stride, N = self.config.dct_width, self.config.dct_stride, x.shape[-1]
+        N_new = math.ceil((N - width) / stride) * stride + width
+        if x.ndim == 2:
+            return np.pad(x,((0,0),(0, N_new - N)), 'constant')
+        return np.pad(x,(0, N_new - N), 'constant')
+    
+    @staticmethod
+    def add_certain_noise(s, n, snr):
+        # y = s + k * n, asssume the variance of s and n are both 1, then snr = 10 * lg(1/k**2) = -20 * lg(k), k = 10 ** (-snr/20)
+        y = s + n * 10 ** (-snr/20)
+        return y
+    
+    @staticmethod
+    def cal_snr(s, n):
+        return 10 * np.log10(np.var(s) / np.var(n))
+
+
+
+class TrainDataset(MyDataset):
+    def __init__(self, config):
+        super(TrainDataset, self).__init__(config, is_train=True)
+
+    def __getitem__(self, idx):
+        s, n = self.s[idx], self.n[np.random.choice(len(self.n))]
+        y = self.add_certain_noise(s, n, np.random.choice(np.arange(-10, 10, 0.1)))
+        return s, y
+
+
+class TestDataset(MyDataset):
+    def __init__(self, config):
+        super(TestDataset, self).__init__(config,  is_train=False)
+
+    def __getitem__(self, idx):
+        s = self.s[idx]
+        return s
---
+++ readme.md
@@ -0,0 +1,21 @@
+## Project description
+This project is a python implementation of the our work:`DeepSparser: Denoising Seismic Signal by Integrating Double Sparsity Model and Denoising Autoencoder`, which is submitted and under peer reviewing. Once the paper is published, we will update the readme file.
+In this project, we provide the model structure, synthetic and real dataset,config files, as well as training and inference of our method both for real and synthetic data.
+
+
+## Project structure
+- `model/` 
+    - `network.py`: define the network structure
+    - `model_real.pt`: the trained model on domain data
+    - `model_synthetic.pt`: the trained model on synthetic data
+- `dataset/`
+    - `real/`: real domain data
+    - `synthetic/`: synthetic data
+    - `dataset_real.py`: load real domain data
+    - `dataset_synthetic.py`: load synthetic data
+- `config/`
+  - `config_real.yaml`: config for real domain data
+  - `config_synthetic.yaml`: config for synthetic data
+- `main_real.ipynb`: the main file for training and testing the model on real domain data
+- `main_synthetic.ipynb`: the main file for training and testing the model on synthetic data
+
